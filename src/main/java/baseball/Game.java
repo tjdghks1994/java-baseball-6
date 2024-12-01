e1e594242cdeb7e1e8edd4c710ab70f88da9aaea
sunghwan <tjdghks130907@gmail.com>
2024-12-01 20:27:20 +0900

    refactor: 비즈니스 로직을 의미 있는 메서드로 추출

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -21,7 +21,19 @@ public class Game {
 
     public void start() {
         init(); // 초기화
+        String answer = proceed();  // 게임 진행
+        finish(answer);     // 게임 완료
+    }
 
+    private void finish(String answer) {
+        if (answer.equals("1")) {
+            start();    // 재시작
+        } else {
+            System.out.println("게임 종료");
+        }
+    }
+
+    private String proceed() {
         while (!complete) {
             System.out.print("숫자를 입력해주세요 : ");
             String number = Console.readLine();
@@ -37,12 +49,7 @@ public class Game {
 
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String answer = Console.readLine();
-
-        if (answer.equals("1")) {
-            start();    // 재시작
-        } else {
-            System.out.println("게임 종료");
-        }
+        return answer;
     }
 
     private void init() {
